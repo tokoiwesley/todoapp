88803d3b2f8e1d33bac702168e2ccb3d237d4ddb
Wesley Tokoi <tokoiwesley@gmail.com>
2022-06-16 00:06:00 +0300

    Go to a homepage for a particular todo list

--- app.py
+++ app.py
@@ -78,6 +78,11 @@ def set_completed_todo(todo_id):
     return redirect(url_for('index'))
 
 
-@app.route('/', methods=['GET'])
+@app.route('/lists/<list_id>', methods=['GET'])
+def get_list_todos(list_id):
+    return render_template('index.html', data=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
+
+@app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
